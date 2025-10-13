e40f59d35e8ff64eb12022e792f8bb7089dd950d
Alen Joby <alenjoby955@gmail.com>
2025-10-13 23:41:43 +0400

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const User = require('./models/user');
+const auth = require('./middleware/auth');
 
 const app = express();
 const port = 3000;
@@ -12,13 +13,16 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/mywebapp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/STUDIOALJO', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 // Routes
 const users = require('./routes/users');
 app.use('/users', users);
+
+// Admin routes
+app.use('/admin', auth, express.static(path.join(__dirname, 'public/admin')));
 
 // Login route
 app.post('/login', async (req, res) => {
